435d3d573ddaa4bb760aa368c8efd58400ca1ea5
uchida-masanori <76242252+uchida-masanori@users.noreply.github.com>
2021-07-02 23:08:04 +0900

    SetDeployInfo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  apiKey: "AIzaSyC1IMcX6RNzvwIxRiRHmDhUQRx1zSbdkAg",
+  authDomain: "my-portfolio-383c0.firebaseapp.com",
+  projectId: "my-portfolio-383c0",
+  storageBucket: "my-portfolio-383c0.appspot.com",
+  messagingSenderId: "63632236687",
+  appId: "1:63632236687:web:dca1db37de336cd0fe50fa",
+  measurementId: "G-W8VYGL2P4D"
+ }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyC1IMcX6RNzvwIxRiRHmDhUQRx1zSbdkAg",
+  authDomain: "my-portfolio-383c0.firebaseapp.com",
+  projectId: "my-portfolio-383c0",
+  storageBucket: "my-portfolio-383c0.appspot.com",
+  messagingSenderId: "63632236687",
+  appId: "1:63632236687:web:dca1db37de336cd0fe50fa",
+  measurementId: "G-W8VYGL2P4D"
+ }
 };
 
 /*
